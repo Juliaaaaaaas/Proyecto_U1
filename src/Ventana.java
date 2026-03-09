e59204b1a9a7dffa02513b7df1a48e4498c27ae4
s24030124-lgtm <s24030124@itsch.edu.mx>
2026-03-08 18:02:50 -0600

    Update Ventana.java
    agregue atributos

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,6 +1,15 @@
 import javax.swing.*;
 
 public class Ventana extends JFrame {
+   private JButton btns[];
+    private JComboBox chamarras;
+    private JRadioButton talla, chica,mediana,grande;
+   private JCheckBox envio, carrito;
+   private JTextArea pedido;
+   private JButton extra, agregar, limpiar;
+   
+   private Container contenedor;
+   
    public Ventana(){
 
         super("Tienda de Chamarras");
